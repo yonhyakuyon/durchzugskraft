cbb4bdff890aad707dde349a050c8abd457b19c4
Dimitry Kriushenkov <76652327+yonhyakuyon@users.noreply.github.com>
2025-05-22 21:56:29 +0300

    Delete Durchzugskraft directory

--- Durchzugskraft/views/layout/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="about-corporate-footer">
-
-    <p>© 2025 BMW E39 Parts Professional</p>
-    <div class="about-footer-text">
-        Официальный партнёр программы BMW Classic
-    </div>
-
-</div>
-<script src="/js/cart-counter.js"></script>
--- Durchzugskraft/views/site/order.php
+++
@@ -1,6 +0,0 @@
-<div class="result-order">
-    <? print_r($order) ;?>
-    <a class=back-direct href="/index.php">Вернуться на главную</a>
-    <a class=pa-direct href="/pa">Перейти в личный кабинет</a>
-</div>
-<script src="/js/jquery-3.7.1.js"></script>
